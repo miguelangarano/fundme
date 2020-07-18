433ec7c621caebc25fc7c4476da94d0fe0546e91
Miguel Langarano <miguelangarano@gmail.com>
2020-07-18 01:07:56 -0500

    screens

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/projects', function () {
+    return view('projects');
+});
+
+Route::get('/registr', function () {
+    return view('registr');
+});
+
+Route::get('/logn', function () {
+    return view('logn');
+});
