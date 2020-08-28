237d372963d628738dc4dcdd600831116ca07e4f
Miguel Langarano <miguelangarano@gmail.com>
2020-08-27 23:11:28 -0500

    com

--- app/Http/Controllers/PaymentData.php
+++ app/Http/Controllers/PaymentData.php
@@ -6,5 +6,62 @@ use Illuminate\Http\Request;
 
 class PaymentData extends Controller
 {
-    //
+    public function createDataToken(){
+        $stripe = new \Stripe\StripeClient('sk_test_BQokikJOvBiI2HlWgH4olfQ2');
+        $customer = $stripe->customers->create([
+            'description' => 'example customer',
+            'email' => 'email@example.com',
+            'payment_method' => 'pm_card_visa',
+        ]);
+        echo $customer;
+
+        $curl = new \Stripe\HttpClient\CurlClient();
+        $curl->setTimeout(10); // default is \Stripe\HttpClient\CurlClient::DEFAULT_TIMEOUT
+        $curl->setConnectTimeout(5); // default is \Stripe\HttpClient\CurlClient::DEFAULT_CONNECT_TIMEOUT
+
+        echo $curl->getTimeout(); // 10
+        echo $curl->getConnectTimeout(); // 5
+
+        // tell Stripe to use the tweaked client
+        \Stripe\ApiRequestor::setHttpClient($curl);
+        $curl = new \Stripe\HttpClient\CurlClient([CURLOPT_PROXY => 'proxy.local:80']);
+        // tell Stripe to use the tweaked client
+        \Stripe\ApiRequestor::setHttpClient($curl);
+
+        \Stripe\Stripe::setLogger($logger);
+
+        $customer = $stripe->customers->create([
+            'description' => 'example customer',
+        ]);
+        echo $customer->getLastResponse()->headers['Request-Id'];
+
+
+        $curl = new \Stripe\HttpClient\CurlClient([CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1]);
+        \Stripe\ApiRequestor::setHttpClient($curl);
+
+        $customers = $stripe->customers->all([],[
+            'api_key' => 'sk_test_...',
+            'stripe_account' => 'acct_...'
+        ]);
+        
+        $stripe->customers->retrieve('cus_123456789', [], [
+            'api_key' => 'sk_test_...',
+            'stripe_account' => 'acct_...'
+        ]);
+
+
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+          );
+          $stripe->charges->create([
+            'amount' => 2000,
+            'currency' => 'usd',
+            'source' => 'tok_amex',
+            'description' => 'My First Test Charge (created for API docs)',
+          ]);
+    }
+
+    public function encryptCardData(){
+        
+    }
 }
--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -13,7 +13,7 @@ class ProjectController extends Controller
         $factory = (new Factory)->withServiceAccount(__DIR__.'/firebase/firebase.json');
         $database = $factory->createDatabase();
         $fecha = new DateTime();
-        $project = new Project($request["name"], $request["description"], $request["expectedIncome"], 0, false, false, $request["videoUrl"], $request["projectPhase"], $request["address"], $request["sriId"], $request["founders"], 0, $fecha->getTimestamp());
+        $project = new Project($request["name"], $request["description"], $request["expectedIncome"], 0, false, false, $request["videoUrl"], $request["projectPhase"], $request["address"], $request["sriId"], $request["founders"], 0, $fecha->getTimestamp(), $request["mission"], $request["vision"]);
         $reference = $database->getReference('projects')->push()->getKey();
         $project->id = $reference;
         $database->getReference('projects')->set($project->exportToJson());
@@ -21,6 +21,20 @@ class ProjectController extends Controller
         $responseData->message = "Proyecto creado con éxito";
         $responseData->code = 0;
         $responseData->body = $reference;
+        return json_encode($responseData);
+    }
+
+    public function getProject(Request $request){
+        $factory = (new Factory)->withServiceAccount(__DIR__.'/firebase/firebase.json');
+        $database = $factory->createDatabase();
+        $reference = $database->getReference('projects/'+$request->projectId);
+        $snapshot = $reference->getSnapshot();
+        $value = $snapshot->getValue();
+        $key = $snapshot->getKey();
+        $fecha = new DateTime();
+        $project = new Project($request["name"], $request["description"], $request["expectedIncome"], 0, false, false, $request["videoUrl"], $request["projectPhase"], $request["address"], $request["sriId"], $request["founders"], 0, $fecha->getTimestamp(), $request["mission"], $request["vision"]);
+        $project->id = $key;
+        $project->id = $reference;
         return json_encode($responseData);
     }
 }
